4aa070e0c6feb85e0bfa5a7971ae104ea0b93b8b
Shipper <info@shippingsoon.com>
2014-06-21 20:49:26 -0400

    added a readme

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -33,6 +33,21 @@
 				<ul class="proficiency-list list-unstyled">
 					<li>
 						C
+						<span class="pull-right">
+							<a href="https://github.com/shippingsoon/IRC-Client" data-placement="left" title="An IRC Client coded in C. This project was completed in 2010.">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://github.com/shippingsoon/Port-Scanner" data-placement="left" title="Demonstration of a multithreaded port scanner in C">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://github.com/shippingsoon/HTTP-Server-and-Client" data-placement="left" title="An HTTP/1.1 Server and Client coded in C.">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
 					</li>
 					<li>
 						C++
@@ -42,7 +57,59 @@
 							</a>
 						</span>
 					</li>
-					<li>PHP</li>
+					<li>
+						PHP
+						<span class="pull-right">
+							<a href="https://github.com/shippingsoon/Shippingsoon" data-placement="left" title="Freelance website built with Codeigniter">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/carcarepeoplecom/7" data-placement="left" title="Car Care People">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/favorbitecom/5" data-placement="left" title="Favorbite">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/ssuhousinghealthcom/1" data-placement="left" title="SSU Housing Health">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/vadenusedcarscom/9" data-placement="left" title="Vaden Used Cars">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/revlinkautocom/8" data-placement="left" title="Revlink">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/chevroletcamarosavannahcom/6" data-placement="left" title="Automotive lead generation">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/ssualphascom/2" data-placement="left" title="Freelance project for an SSU fraternity.">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/talkingphotomagnetcom/4" data-placement="left" title="eCommerce website for photography">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+						<span class="pull-right">
+							<a href="https://www.shippingsoon.com/portfolio/shareideasme/3" data-placement="left" title="Social network for sharing and collaborating">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+					</li>
 					<li>
 						JavaScript/Node.js
 						<span class="pull-right">
@@ -70,7 +137,14 @@
 						</span>
 					</li>
 					<li>SQL (MySQL)</li>
-					<li>HTML5 (CSS3, Canvas)</li>
+					<li>
+						HTML5 (CSS3, Canvas)
+						<span class="pull-right">
+							<a href="http://localhost/www.shippingsoon.com/portfolio/freelancedeskcom/11" data-placement="left" title="Freelance Desk">
+								<span class="glyphicon glyphicon-star"></span>
+							</a>
+						</span>
+					</li>
 					<li>Bash</li>
 					<li>Design Patterns (Model View Controller, State pattern)</li>
 				</ul>
---
+++ readme.md
@@ -0,0 +1,45 @@
+Shippingsoon
+=======
+
+<div>
+	<p>
+		Shippingsoon is a free and open source content management system I designed in 2012 to host my freelance work and software projects. In 2014 it was redesigned from scratch with Codeigniter, jQuery and Twitter Bootstrap.
+	</p>
+	<p>
+		Like most of my open source endeavours Shippingsoon will remain in perpetual development until it reflects the best programming
+		<a href="https://en.wikipedia.org/wiki/Best_coding_practices" target="_blank">practices</a> and
+		<a href="https://en.wikipedia.org/wiki/Category:Programming_principles" target="_blank">principles</a>.
+		Considering the fact that technology standards are constantly changing and new practices are being published each day, development on Shippingsoon will never end.
+	</p>
+	<p>
+		Most <a href="http://www.merriam-webster.com/dictionary/busy" title="Busy" data-toggle="tooltip">self</a>-<a href="http://www.merriam-webster.com/dictionary/lazy" title="Lazy">respecting</a> developers would just grab a WordPress template for their personal website and call it a day, but I am no ordinary developer.
+		Feel free to fork the source code or play around with our fancy <a href="https://www.shippingsoon.com/user/login"> login terminal</a>. Shippingsoon would never have happened without the help of <a href="https://www.shippingsoon.com/about/credits">these open source projects</a>.
+	</p>
+	<h3>Features:</h3>
+	<ul>
+		<li>
+			Each page is built with a responsive design. <b>Try</b> resizing your web browser's window to see it in action.
+		</li>
+		<li>
+			The <a href="portfolio/search/0/12/code">search page</a> was coded with HTML5 pushstate and AJAX for a modern search experience.
+		</li>
+		<li>
+			Shippingsoon's Model View Controller design pattern allows for the codebase to be easily maintained and refined.
+		</li>
+		<li>
+			For <strong>SEO</strong> (Search Engine Optimization): 
+			<ul>
+				<li>I routed URL segments to make them smaller (and ordered by relevance).</li>
+				<li>Each page title/description is unique.</li>
+				<li>H1 and <strong>strong tags</strong> were used in moderation.</li>
+				<li>Every article has search tags</li>
+			</ul>
+		</li>
+		<li>
+			Each line of code was written with the DRY (Don't Repeat Yourself) principle in mind. 
+		</li>
+		<li>
+			Did I mention that each page is <a href="http://html5.validator.nu/?doc=https%3A%2F%2Fwww.shippingsoon.com%2F" target="_blank">valid HTML5</a> with <a href="http://csswizardry.com/2011/09/writing-efficient-css-selectors/" target="_blank">efficient CSS</a> selectors.
+		</li>
+	</ul>
+</div>
